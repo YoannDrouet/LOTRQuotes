02093c24e82dd61d4d1a35344047ee30103bb6ac
ydrouet <yoanndrouet@hotmail.fr>
2023-10-13 16:35:53 +0200

    essaie 2 manifest.json

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register("/../serviceworker.js");
+}
+
 let favList = [];
 const favButton = document.getElementById("viewFavorite");
 homePage();
